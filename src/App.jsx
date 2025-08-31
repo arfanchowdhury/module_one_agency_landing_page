6c98302d30f3ffd309662f180138cb492c24ac4f
arfan_chowdhury <arfanchowdhury.cse@gmail.com>
2025-09-01 03:48:33 +0600

    Hero Secetion & Companies Logo Completed, fix Navbar render key props problem

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+export default function App(){
+  return (
+    <div>
+      <div className="flex items-center justify-center p-8 m-4 bg-emerald-950 border-2 border-lime-500 rounded-s-md">
+        <h1 className="text-4xl text-white">Welcome To Taildwind V4</h1>
+      </div>
+    </div>
+  );
+}
